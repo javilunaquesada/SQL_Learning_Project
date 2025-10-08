2c5f085adb922cfcd226c7e98e8c4310a5f4954a
Javier Luna <javilunaquesada@gmail.com>
2025-10-08 14:34:19 +0200

    query 5

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,53 @@
+/*
+Answer: What are the most optimmal skills to learn (aka it's in demand and pays well)?
+- Identify skills in high demand and associated with high average salaries for Data Analyst roles
+- Concentrates on remote positions with specified salaries
+- Why? target skills that offer job security (high demand) and financial benefits (high salaries), 
+    offering a balanced approach for job seekers aiming to enhance their career prospects
+- We can use CTE (Common Table Expressions) to break down the problem into manageable parts (using
+  previous queries 3, 4 as subqueries)
+*/
+
+WITH skills_demand AS (
+    SELECT 
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM
+        job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_postings_fact.job_title_short = 'Data Analyst' AND
+        job_postings_fact.salary_year_avg IS NOT NULL AND
+        job_postings_fact.job_work_from_home = TRUE
+    GROUP BY skills_dim.skill_id
+), average_salary AS(
+    SELECT 
+        skills_job_dim.skill_id,
+        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS average_salary   -- Rounded to nearest whole number
+    FROM
+        job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_postings_fact.job_title_short = 'Data Analyst' AND
+        job_postings_fact.salary_year_avg IS NOT NULL AND
+        job_postings_fact.job_work_from_home = TRUE
+    GROUP BY skills_job_dim.skill_id
+)
+
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    average_salary
+FROM skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE 
+        demand_count>10  -- Filter to skills with more than 10 job postings
+ORDER BY 
+        average_salary DESC,
+        demand_count DESC
+        
+LIMIT 25
